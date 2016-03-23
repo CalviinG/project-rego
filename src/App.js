966a0c88277897406450240dba13630cd9480e02
Dan Andersson <dan@bilprospekt.se>
2016-03-23 17:48:51 +0100

    Built base for navigation.

--- src/App.js
+++ src/App.js
@@ -3,33 +3,74 @@ import React, { Component } from 'react';
 // Components
 import NavigationComponent from './components/navigation_component.js';
 
-export default class App extends Component {
+const App = React.createClass({
+    getInitialState() {
+        return {
+            activeMain: 0,
+            activeSub: 0,
+        };
+    },
+
+    _onViewChange(type, index) {
+        if (type === 'main') {
+            this.setState({ 
+                activeMain: index,
+                activeSub: 0,
+            });
+        } else if (type === 'sub') {
+            this.setState({ activeSub: index });
+        }
+    },
+
   	render() {
-  		const navObject = {
-  			link1: {
-  				label: 'Play',
-  				subLinks: {
-  					subLink1: 'Casual',
-  					subLink2: 'Deathmatch',
-  					subLink3: 'Matchmaking',
-  					subLink4: 'Browse Servers',
-  					subLink5: 'Competative Play',
-  				},
-  			},
-  			link2: {
-  				label: 'Weapons',
-  				subLinks: {
-  					subLink1: 'Inventory',
-  					subLink2: 'Marketplace',
-  					subLink3: 'Shop', 
-  				}
-  			},
-  		};
+  		const linksData = [
+            [
+                'Play',
+                'Casual',
+                'Arms Race',
+                'Demolition',
+                'Deathmatch',
+                'Matchmaking',
+                'Competative Play',
+                'Browse',
+            ],
+            [
+                'Inventory',
+                'Loadout',
+                'Skins',
+                'Crates',
+            ],
+            [
+                'Store',
+                'Marketplace',
+                'Keys',
+                'Crates',
+            ],
+            [
+                'Watch',
+                'Twitch',
+                'Matchmaking',
+                'Tournaments',
+            ],
+            [
+                'Settings',
+                'In-game',
+                'Video',
+                'Advanced',
+            ],
+        ];
 
 	    return (
 	      	<div className='app-wrapper'>
-	      		<NavigationComponent links={navObject} />
+	      		<NavigationComponent
+                    linksData={linksData}
+                    activeMain={this.state.activeMain}
+                    activeSub={this.state.activeSub}
+                    onChange={this._onViewChange}
+                    />
 	      	</div>
 	    );
-  	}
-}
+  	},
+});
+
+export default App;
--- src/components/navigation_component.js
+++ src/components/navigation_component.js
@@ -1,25 +1,74 @@
-import React from 'react';
+import React      from 'react';
+import _          from 'underscore';
+import classNames from 'classnames';
 
 const NavigationComponent = React.createClass({
     propTypes: {
-        links: React.PropTypes.object.isRequired,
+        linksData:  React.PropTypes.array.isRequired,
+        activeMain: React.PropTypes.number.isRequired,
+        activeSub:  React.PropTypes.number.isRequired,
     },
 
     getInitialState() {
         return {
-            selectedMainLink: 0,
-            selectedSubLink: 0,
+            activeMain: this.props.activeMain,
+            activeSub: this.props.activeSub,
         };
     },
 
+    _onMainClick(index) {
+        if (typeof this.props.onChange === 'function') {
+            this.props.onChange('main', index);
+        }
+
+        this.setState({
+            activeMain: index,
+            activeSub: 0,
+        });
+    },
+
+    _onSubClick(index) {
+        if (typeof this.props.onChange === 'function') {
+            this.props.onChange('sub', index);
+        }
+
+        this.setState({ activeSub: index });
+    },
+
     render() {
+        const mainLinks = _(this.props.linksData).map((key, value) => {
+            const mainClass = classNames('main-link', {
+                'active-link': (value === this.state.activeMain),
+            });
+            return <p className={mainClass} key={value} onClick={this._onMainClick.bind(this, value)}>{key[0]}</p>;
+        });
+
+        let subLinks = null;
+        _(this.props.linksData).map((key, value) => {
+            if (value === this.state.activeMain) {
+                subLinks = _(key).map((value, index) => {
+                    if (index > 0) {
+                        const subClass = classNames('sub-link', {
+                            'active-link': ((index - 1) === this.state.activeSub),
+                        });
+                        return <p className={subClass} key={index} onClick={this._onSubClick.bind(this, (index - 1))}>{value}</p>;
+                    }  
+                });
+            }
+            
+        });
+
         return (
             <div className='navigation'>
                 <div className='navigation__main'>
-                    Main
+                    <i className='fa fa-angle-left' />
+                    <i className='fa fa-angle-right' />
+                    <i className='fa fa-steam' />
+                    {mainLinks}
+                    <i className='fa fa-sign-out' />
                 </div>
                 <div className='navigation__sub'>
-                    Sub
+                    {subLinks}
                 </div>
             </div>
         );
