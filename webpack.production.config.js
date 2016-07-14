eb1862683ffbaf82943c170463842b7f1bc430d3
Dan Andersson <danandersson92@gmail.com>
2016-07-14 16:48:25 +0200

    Fixed build script
    Added a build script to gh-pages.

---
+++ webpack.production.config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    path.join(__dirname, '/src/index')
+  ],
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: path.join(__dirname, 'www/index.html')
+    }),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      'project-rego': path.resolve(__dirname, '../src')
+    },
+    modulesDirectories: [
+      "node_modules",
+      path.resolve(__dirname, "node_modules"),
+      path.resolve(__dirname, '../src'),
+      path.resolve(__dirname, '../node_modules'),
+    ],
+  },
+  devServer: {
+    contentBase: 'build',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['react-hot', 'babel'], include: path.join(__dirname, 'src') },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.(jpg|png)$/, loader: 'file-loader'}
+    ]
+  }
+};
